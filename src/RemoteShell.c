5c2a25b2a564dc77c3fba23a352193aa4ed41f7d
jungjin0003 <jungjin00031@gmail.com>
2021-04-20 12:49:34 +0900

    src update

--- src/RemoteShell.c
+++ src/RemoteShell.c
@@ -1,1 +1,36 @@
 #include "RemoteShell.h"
+
+int initialization()
+{
+    WSADATA wsadata;
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0)
+    {
+        printf("[-] WSAStartup failed!\n");
+        printf("[+] GetLastError : %d\n", GetLastError());
+        return 0;
+    }
+}
+
+int ListenShell(char *path, SOCKADDR_IN sockaddr)
+{
+
+}
+
+int ConnectShell(char *path, SOCKADDR_IN sockaddr)
+{
+    SOCKET sock = NULL;
+    
+    if (initialization() == 0)
+    {
+        return 0;
+    }
+
+    sock = socket(sockaddr.sin_family, SOCK_STREAM, IPPROTO_TCP);
+
+    if (sock == INVALID_SOCKET)
+    {
+        printf("[-] Create socket failed!\n");
+        printf("[+] GetLastError : %d\n", GetLastError());
+    }
+}
