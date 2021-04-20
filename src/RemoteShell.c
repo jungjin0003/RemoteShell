9cb495834f4d18a1b7e7417be44db87b10aaafcb
jungjin0003 <jungjin00031@gmail.com>
2021-04-20 12:59:07 +0900

    src update

--- src/RemoteShell.c
+++ src/RemoteShell.c
@@ -14,7 +14,21 @@ int initialization()
 
 int ListenShell(char *path, SOCKADDR_IN sockaddr)
 {
+    SOCKET sock = NULL;
+    
+    if (initialization() == 0)
+    {
+        return 0;
+    }
 
+    sock = socket(sockaddr.sin_family, SOCK_STREAM, IPPROTO_TCP);
+
+    if (sock == INVALID_SOCKET)
+    {
+        printf("[-] Create socket failed!\n");
+        printf("[+] GetLastError : %d\n", GetLastError());
+        return 0;
+    }
 }
 
 int ConnectShell(char *path, SOCKADDR_IN sockaddr)
@@ -32,5 +46,13 @@ int ConnectShell(char *path, SOCKADDR_IN sockaddr)
     {
         printf("[-] Create socket failed!\n");
         printf("[+] GetLastError : %d\n", GetLastError());
+        return 0;
+    }
+
+    if (connect(sock, (const struct sockaddr *)&sockaddr, sizeof(sockaddr)) == 0)
+    {
+        printf("[-] %s:%d Connect failed!\n", inet_ntoa(sockaddr.sin_addr), htons(sockaddr.sin_port));
+        printf("[+] GetLastError : %d\n", GetLastError());
+        return 0;
     }
 }
